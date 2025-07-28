cfe9894b244746ce9d447116fcfaa47251d674b7
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-28 13:11:11 -0500

    feat(wallet): implement wallet screen with total balance display and navigation to wallet modal feat(walletModal): create wallet modal component

---
+++ app/(modals)/walletModal.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ModalWrapper from '@/components/shared/ModalWrapper'
+
+const walletModal = () => {
+  return (
+    <ModalWrapper>
+      <Text>walletModal</Text>
+    </ModalWrapper>
+  )
+}
+
+export default walletModal
+
+const styles = StyleSheet.create({})
--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,11 +1,48 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { View } from "moti";
+import Typo from "@/components/ui/Typo";
+import * as Icons from "phosphor-react-native";
+import { useRouter } from "expo-router";
 
 const wallet = () => {
+  const router = useRouter();
+  const getTotalBalance = () => {
+    return 5000;
+  };
+
   return (
-    <ScreenWrapper style={styles.container}>
-      <Text style={styles.text}>wallet</Text>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
+      <View style={styles.container}>
+        <View style={styles.balanceView}>
+          <View style={{ alignItems: "center" }}>
+            <Typo size={45} fontWeight={"500"}>
+              ${getTotalBalance()?.toFixed(0)}
+            </Typo>
+            <Typo size={16} color={colors.neutral300}>
+              Total Balance
+            </Typo>
+          </View>
+        </View>
+        <View style={styles.wallets}>
+          <View style={styles.flexRow}>
+            <Typo size={20} fontWeight={"600"}>
+              MIS BILLETERAS
+            </Typo>
+            <TouchableOpacity onPress={() => router.push("/(modals)/walletModal")}>
+              <Icons.PlusCircleIcon
+                size={verticalScale(20)}
+                weight="bold"
+                color={colors.white}
+              />
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
@@ -15,11 +52,30 @@ export default wallet;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
+  },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 20,
-    color: "#333",
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._20,
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopRightRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingX._25,
+  },
+  lisStyle: {
+    paddingVertical: spacingY._25,
+    paddingTop: spacingY._15,
   },
 });
